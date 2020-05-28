6c7bd62246c1f0a29e834a7512599476156d932b
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-05-28 14:23:48 +0900

    [update]App: variables
    変数や定数に関して学習

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
+    /* mainメソッド */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); // 標準出力
+
+        /* 型 */
+        /* 整数 */
+        long bigInt = 100000000L;
+        int integer = 150000;
+        short smallInteger = 30000;
+        byte byteInteget = 100;
+        /* 小数 */
+        double weight = 171.2;
+        float weight = 67.5F;
     }
 }
