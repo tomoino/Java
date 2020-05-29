1ed64e926f447cdb5b9ae2f630792bd557b08027
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-05-29 23:31:56 +0900

    [update]index
    各種関数を追加

--- src/App.java
+++ src/App.java
@@ -3,14 +3,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!"); // 標準出力
 
-        /* 型 */
+        /* 変数 */
         /* 整数 */
         long bigInt = 100000000L;
         int integer = 150000;
         short smallInteger = 30000;
         byte byteInteget = 100;
-        /* 小数 */
+        /* 小数：真に厳密な計算ができないことに注意する */
         double weight = 171.2;
         float weight = 67.5F;
+        /* その他 */
+        boolean exists = true;
+        char gender = '男';
+        String hanzi = "漢字"
+        
+        /* 定数: finalをつける */
+        final double TAX = 1.10;
     }
 }
