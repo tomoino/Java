abeed85f9534341f2927a0534587beac2fe8cde7
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-05-31 17:03:12 +0900

    [update]app
    配列について学習

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     /* mainメソッド */
     public static void main(String[] args) throws Exception {
-        /* 変数 */
+        /* 変数：変数の寿命はブロックが終了するまで。 */
         /* 整数 */
         long bigInt = 100000000L; // long型は最後にLを付ける
         int integer = 150000;
@@ -14,6 +14,7 @@ public class App {
         boolean exists = true;
         char gender = '男'; // charは引用符
         String hanzi = "漢字"; // Stringは二重引用符
+        int strLen = hanzi.length(); // length()を付けると文字数を取得できる
         
         /* 定数: finalをつける */
         final double TAX = 1.10;
@@ -41,5 +42,24 @@ public class App {
             System.out.println("Small");
         }
         // switch, while, forはCと同様。
+
+        /* 配列 */
+        int[] arr; // 型名[]で宣言。int型の配列がメモリ上に作成
+        // arr = new int[5]; // int型の要素を5個作成する。
+        // arr = new int[5][10]; // 多次元配列も生成できる。
+        arr = new int[] { 0, 1, 2, 3, 4}; // 配列の初期化。arrには配列の先頭要素のアドレスが代入される。
+        System.out.println(arr.length); // 配列の長さ
+        for (int value : arr) { // 配列の要素を順に取り出すときの書き方
+            System.out.println(value);
+        }
+        int[] arr2 = arr; // arr2はarrと同じアドレスになる。
+        /* ガベージコレクション：
+        　　    newで確保された要素の寿命はブロックが終了しても続くため、本来Cのようにメモリ解放をしなくてはいけないが 、
+                Javaではどの変数からも参照されなくなったメモリ領域を自動的に解放してくれる。
+        */
+        /* nullについて；
+            nullを代入すると参照型の変数はどこも参照していない状態になる。
+        */
+
     }
 }
