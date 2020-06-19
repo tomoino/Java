7b467b77524506749e3959377881a45e9b186b9f
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-06-19 22:29:17 +0900

    [update]object
    静的メンバなどについて学習

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // インスタンスの生成
-        Object obj = new Object();
-        obj.name = "object1";
+        Object obj = new Object("object1"); // objにはインスタンスobjのために確保されたメモリの先頭番地が入っている
         obj.incValue();
         obj.printValue(); 
     }
--- src/Object.java
+++ src/Object.java
@@ -2,6 +2,18 @@ public class Object {
     // Javaではクラス内で宣言された変数をフィールドという
     String name;
     int value;
+    // 静的メンバ：同じクラスからできたインスタンスで共有される
+    // クラス名.静的メンバ名で呼び出せる Object.objectNum
+    static int objectNum; 
+
+    // コンストラクタ  
+    Object(String name) {
+        this.name = name;
+    }
+    // コンストラクタでもオーバーロードは可能
+    Object() {
+        this("default"); // 自分の他のコンストラクタを呼べる 
+    }
 
     // メソッド
     void printValue() {
