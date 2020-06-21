0abbae9535ec366a694420368648b06e27d71d09
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-06-21 10:43:51 +0900

    [update]object: access control
    メンバに対するアクセス制御について学習

--- src/Object.java
+++ src/Object.java
@@ -1,7 +1,7 @@
 public class Object {
     // Javaではクラス内で宣言された変数をフィールドという
-    String name;
-    int value;
+    private String name;
+    private int value;
     // 静的メンバ：同じクラスからできたインスタンスで共有される
     // クラス名.静的メンバ名で呼び出せる Object.objectNum
     static int objectNum; 
@@ -16,16 +16,33 @@ public class Object {
     }
 
     // メソッド
-    void printValue() {
+    public void printValue() {
         System.out.println(this.value);
     }
 
-    void incValue() {
+    public void incValue() {
         this.value++;
     }
 
     // 静的メソッド：静的メソッド内では静的メンバしか使えない。
-    static void printObjectNum() {
+    public static void printObjectNum() {
         System.out.println(Object.objectNum);
+    }
+
+    // getter / setter
+    public String getName() {
+        return this.name;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 }
